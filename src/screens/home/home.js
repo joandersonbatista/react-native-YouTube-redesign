1ca6e8ab42ba38b319002a3197a6126f68094983
Joanderson Batista <joandersonbatista.br@gmail.com>
2021-07-20 19:12:08 -0300

    blabla

---
+++ src/screens/home/home.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Header from "./header"
+import {
+    View
+} from "react-native"
+import BottomNavigation from "./BottomNavigation"
+
+export default () => {
+    return (
+        <>
+            <Header />
+            <View style={{flex: 1}}>
+            <BottomNavigation />
+            </View>
+        </>
+    )
+}
